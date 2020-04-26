d3ea46b6e2c66d55192395d663e99e8fb4f8e4e6
Aleksandr Razumov <ar@gortc.io>
2020-04-26 04:39:27 +0300

    add  format

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo 'golangci-lint-action: start'
 cd "${GITHUB_WORKSPACE}/${DIRECTORY}" || exit 1
 
 # shellcheck disable=SC2086
-golangci-lint run --out-format github-actions ${FLAGS}
+golangci-lint run --out-format ${FORMAT} ${FLAGS}

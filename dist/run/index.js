291ea21c8a38f4f35d47ce2f9e01bdb9899adc07
Kamil Samigullin <kamil@samigullin.info>
2022-06-13 23:42:01 +0300

    update dist

--- dist/run/index.js
+++ dist/run/index.js
@@ -1123,6 +1123,8 @@ function createTar(archiveFolder, sourceDirectories, compressionMethod) {
             ...getCompressionProgram(),
             '-cf',
             cacheFileName.replace(new RegExp(`\\${path.sep}`, 'g'), '/'),
+            '--exclude',
+            cacheFileName.replace(new RegExp(`\\${path.sep}`, 'g'), '/'),
             '-P',
             '-C',
             workingDirectory.replace(new RegExp(`\\${path.sep}`, 'g'), '/'),

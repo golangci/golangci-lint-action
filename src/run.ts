f4bf83e572ca31c99f750f6c00b2727425a6e978
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-02-13 21:48:21 +0100

    chore: extract patch related code

--- src/run.ts
+++ src/run.ts
@@ -1,23 +1,15 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
-import { Context } from "@actions/github/lib/context"
 import { exec, ExecOptions } from "child_process"
 import * as fs from "fs"
 import * as path from "path"
-import { dir } from "tmp"
 import { promisify } from "util"
 
 import { restoreCache, saveCache } from "./cache"
 import { install } from "./install"
-import { alterDiffPatch } from "./utils/diffUtils"
+import { fetchPatch, isOnlyNewIssues } from "./patch"
 
 const execShellCommand = promisify(exec)
-const writeFile = promisify(fs.writeFile)
-const createTempDir = promisify(dir)
-
-function isOnlyNewIssues(): boolean {
-  return core.getBooleanInput(`only-new-issues`, { required: true })
-}
 
 type Env = {
   binPath: string
@@ -36,109 +28,6 @@ async function prepareEnv(): Promise<Env> {
   core.info(`Prepared env in ${Date.now() - startedAt}ms`)
 
   return { binPath, patchPath }
-}
-
-async function fetchPatch(): Promise<string> {
-  if (!isOnlyNewIssues()) {
-    return ``
-  }
-
-  const ctx = github.context
-
-  switch (ctx.eventName) {
-    case `pull_request`:
-    case `pull_request_target`:
-      return await fetchPullRequestPatch(ctx)
-    case `push`:
-      return await fetchPushPatch(ctx)
-    case `merge_group`:
-      return ``
-    default:
-      core.info(`Not fetching patch for showing only new issues because it's not a pull request context: event name is ${ctx.eventName}`)
-      return ``
-  }
-}
-
-async function fetchPullRequestPatch(ctx: Context): Promise<string> {
-  const pr = ctx.payload.pull_request
-  if (!pr) {
-    core.warning(`No pull request in context`)
-    return ``
-  }
-
-  const octokit = github.getOctokit(core.getInput(`github-token`, { required: true }))
-
-  let patch: string
-  try {
-    const patchResp = await octokit.rest.pulls.get({
-      owner: ctx.repo.owner,
-      repo: ctx.repo.repo,
-      [`pull_number`]: pr.number,
-      mediaType: {
-        format: `diff`,
-      },
-    })
-
-    if (patchResp.status !== 200) {
-      core.warning(`failed to fetch pull request patch: response status is ${patchResp.status}`)
-      return `` // don't fail the action, but analyze without patch
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    patch = patchResp.data as any
-  } catch (err) {
-    console.warn(`failed to fetch pull request patch:`, err)
-    return `` // don't fail the action, but analyze without patch
-  }
-
-  try {
-    const tempDir = await createTempDir()
-    const patchPath = path.join(tempDir, "pull.patch")
-    core.info(`Writing patch to ${patchPath}`)
-    await writeFile(patchPath, alterDiffPatch(patch))
-    return patchPath
-  } catch (err) {
-    console.warn(`failed to save pull request patch:`, err)
-    return `` // don't fail the action, but analyze without patch
-  }
-}
-
-async function fetchPushPatch(ctx: Context): Promise<string> {
-  const octokit = github.getOctokit(core.getInput(`github-token`, { required: true }))
-
-  let patch: string
-  try {
-    const patchResp = await octokit.rest.repos.compareCommitsWithBasehead({
-      owner: ctx.repo.owner,
-      repo: ctx.repo.repo,
-      basehead: `${ctx.payload.before}...${ctx.payload.after}`,
-      mediaType: {
-        format: `diff`,
-      },
-    })
-
-    if (patchResp.status !== 200) {
-      core.warning(`failed to fetch push patch: response status is ${patchResp.status}`)
-      return `` // don't fail the action, but analyze without patch
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    patch = patchResp.data as any
-  } catch (err) {
-    console.warn(`failed to fetch push patch:`, err)
-    return `` // don't fail the action, but analyze without patch
-  }
-
-  try {
-    const tempDir = await createTempDir()
-    const patchPath = path.join(tempDir, "push.patch")
-    core.info(`Writing patch to ${patchPath}`)
-    await writeFile(patchPath, alterDiffPatch(patch))
-    return patchPath
-  } catch (err) {
-    console.warn(`failed to save pull request patch:`, err)
-    return `` // don't fail the action, but analyze without patch
-  }
 }
 
 type ExecRes = {

bc85e275919550e2bc1db862c6ecfba463cf6ff4
Fernandez Ludovic <ldez@users.noreply.github.com>
2025-02-13 13:26:19 +0100

    tests: add go tool test

---
+++ sample-go-tool/sample.go
@@ -0,0 +1,26 @@
+// Package main is used as test input for golangci action.
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+)
+
+// Hash~
+func Hash(data string) string {
+	retError()
+	retError2()
+
+	h := md5.New()
+	h.Write([]byte(data))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func retError() error {
+	return errors.New("err")
+}
+
+func retError2() error {
+	return errors.New("err2")
+}
